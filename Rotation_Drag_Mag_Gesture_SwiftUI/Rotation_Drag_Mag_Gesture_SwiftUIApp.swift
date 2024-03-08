04b4fa82438331691760286f5922d5328fd1e572
Sandy Gomez <sgomez1029@gmail.com>
2024-03-07 18:49:22 -0800

    Initial Commit

---
+++ Rotation_Drag_Mag_Gesture_SwiftUI/Rotation_Drag_Mag_Gesture_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rotation_Drag_Mag_Gesture_SwiftUIApp.swift
+//  Rotation_Drag_Mag_Gesture_SwiftUI
+//
+//  Created by Sandra Gomez on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Rotation_Drag_Mag_Gesture_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
