9018399e042d7896d3c54c3f4969d0dcafb80402
Sandy Gomez <sgomez1029@gmail.com>
2024-03-07 19:40:34 -0800

    Magnification Gesture SwiftUI (UCSD Extension)

--- Rotation_Drag_Mag_Gesture_SwiftUI/ContentView.swift
+++ Rotation_Drag_Mag_Gesture_SwiftUI/ContentView.swift
@@ -8,19 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var degree = 0.0
+    //Rotation Gesture
+//    @State private var degree = 0.0
+    
+    // Magnification Gesture:
+    @State private var tempValue: CGFloat = 0
+    @State private var finalValue: CGFloat = 1
+    
     var body: some View {
         VStack {
             Image(systemName: "star.fill")
                 .font(.system(size: 200))
                 .foregroundColor(.green)
-                .rotationEffect(Angle.degrees(degree))
+            //Rotation Gesture:
+//                .rotationEffect(Angle.degrees(degree))
+//                .gesture(
+//                    RotationGesture()
+//                        .onChanged({ angle in
+//                            self.degree = angle.degrees
+//                        })
+//            )
+            // Magnification Gesture
+                .scaleEffect(finalValue + tempValue)
                 .gesture(
-                    RotationGesture()
-                        .onChanged({ angle in
-                            self.degree = angle.degrees
-                        })
-            )
+                    MagnificationGesture()
+                        .onChanged { amount in
+                            self.tempValue = amount - 1
+                        }
+                        .onEnded { amount in
+                            self.finalValue += self.tempValue
+                            self.tempValue = 0
+                        }
+                )
         }
     }
 }
